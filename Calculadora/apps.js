3e742fe1388173ab96ac9818a210d659dba5f9b7
Emanuel Arroyo <ema201217@gmail.com>
2022-06-03 14:59:25 -0300

    borre cosas

--- Calculadora/apps.js
+++ Calculadora/apps.js
@@ -1,11 +1,6 @@
 const process = require('process');
 const calculeitor = require ('./calculeitor');
 
-let operacion = process.argv [8];
-let numeroA = +process.argv [3];
-let numeroB = +process.argv [4];
-let resultado = 0;
-
 if ( operacion === "sumar") {
     resultado = calculeitor.sumar (numeroA, numeroB);
 }
@@ -18,7 +13,3 @@ else if ( operacion === "dividir") {
 else if ( operacion === "multiplicar") {
     resultado = calculeitor.multiplicar (numeroA, numeroB);
 }
-else if ( operacion === "potencia") {
-    resultado = calculeitor.potencia (numeroA, numeroB);
-}            
-console.log (o); 
